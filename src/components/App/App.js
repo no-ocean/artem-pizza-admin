4b2aa7bf68d9ccb60df625fce41b0fd9f5ae3297
Borisenko Anton <aborisenko@xbsoftware.by>
2021-07-25 19:12:38 +0300

    [add] main layout, sidebar and top bar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,39 @@
+import { Route, NavLink, Switch } from "react-router-dom";
 import AddForm from "../AddForm";
+import Orders from "../Orders";
+import Home from "../Home";
 
 const App = () => {
 	return (
-		<div className="container">
-			<AddForm />
+		<div className="flex flex-column wide">
+			<div className="topbar bg-top">
+				<h3 className="logo">Artem Pizza Admin Pannel</h3>
+				<div className="flex">
+					<span className="mdi mdi-18px mdi-bell"></span>
+					<span className="topbar__button">Выход</span>
+				</div>
+				
+			</div>
+			<div className="wide-full bg-main flex">
+				<nav className="nav bg-dark">
+					<NavLink exact activeClassName="nav__link_active" className="nav__link" to="/">Главная</NavLink>
+					<NavLink activeClassName="nav__link_active" className="nav__link" to="/ingredients">Ингридиенты</NavLink>
+					<NavLink activeClassName="nav__link_active" className="nav__link" to="/orders">Заказы</NavLink>
+				</nav>
+				<div className="content">
+					<Switch>
+						<Route path="/ingredients">
+							<AddForm />
+						</Route>
+						<Route path="/orders">
+							<Orders />
+						</Route>
+						<Route path="/">
+							<Home />
+						</Route>
+					</Switch>
+				</div>
+			</div>
 		</div>
 	);
 }
