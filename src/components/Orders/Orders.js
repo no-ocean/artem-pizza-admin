c6372dd0a45a7ed7be24b5476d13b9cae95a4ec8
Borisenko Anton <aborisenko@xbsoftware.by>
2021-07-26 00:41:12 +0300

    [dev] display orders in table, loading spinner and error state

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,6 +1,65 @@
+import { useState, useEffect } from "react";
+import Loader from "react-loader-spinner";
+import OrderItem from "../OrderItem";
+import { getData } from "../../api/api"
+
 const Orders = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState();
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        const loadData = async () => {
+            try {
+                const json = await getData("orders");
+                setData(json);
+                setIsLoading(false);
+            } catch(error) {
+                setError(error);
+                setIsLoading(false);
+            }
+        };
+        loadData();
+    }, []);
+
+    if(error) {
+        return <h1>ERROR: {error.message}</h1>
+    }
+
     return (
-        <h1>Orders list:</h1>
+        <>
+            <h1>Orders list:</h1>
+            { 
+                isLoading ? <div className="spinner flex">
+                    <Loader
+                        type="Oval"
+                        color="#22CBF5"
+                        height={20}
+                        width={20}
+                        timeout={30000}
+                    />
+                </div> :
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>ID: </th>
+                            <th>Размер: </th>
+                            <th>Тесто: </th>
+                            <th>Соус: </th>
+                            <th>Ингредиенты: </th>
+                            <th>Имя: </th>
+                            <th>Номер карты: </th>
+                            <th>Адрес: </th>
+                            <th>Цена: </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <OrderItem data={data} />
+                    </tbody>
+                </table>
+            }
+            {JSON.stringify(data)}
+        </>
     );
 }
 
