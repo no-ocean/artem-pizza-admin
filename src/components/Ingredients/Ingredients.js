4276befd33007fa2c018a958e44ff3fde03988b6
Borisenko Anton <aborisenko@xbsoftware.by>
2021-08-14 21:22:45 +0300

    [dev] ingredients page buttons and functionality

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -3,14 +3,16 @@ import Loader from "react-loader-spinner";
 import { getData } from "../../api/api";
 import { useForm } from "react-hook-form";
 import EditForm from "../EditForm";
+import AddForm from "../AddForm";
 
 const Ingredients = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState();
     const [data, setData] = useState();
     const [editForm, setEditForm] = useState(false);
+    const [addForm, setAddForm] = useState(false);
 
-    const { register, handleSubmit, watch } = useForm();
+    const { register, watch } = useForm();
 
     const imgPath = "//localhost:4000/";
 
@@ -18,11 +20,11 @@ const Ingredients = () => {
         const loadData = async () => {
             try {
                 const json = await getData("v1", "ingredients");
-                // const normalizeJson = json.reduce((acc, item) => {
-                //     acc[item.slug] = item;
-                //     return acc;
-                // }, {});
-                setData(json);
+                const normalizeJson = json.reduce((acc, item) => {
+                    acc[item.slug] = item;
+                    return acc;
+                }, {});
+                setData(normalizeJson);
                 setIsLoading(false);
             } catch (error) {
                 setError(error);
@@ -30,7 +32,7 @@ const Ingredients = () => {
             }
         }
         loadData();
-    }, []);
+    }, [editForm, addForm]);
 
     if (error) {
         return <h1>ERROR: {error.message}</h1>
@@ -57,12 +59,26 @@ const Ingredients = () => {
         setEditForm(true);
     }
 
+    const handleAdd = () => {
+        setAddForm(true);
+    }
+
     const handleCancel = (e) => {
         e.preventDefault();
         setEditForm(false);
+        setAddForm(false);
     }
 
-    const items = data.map((item) => {
+    const handleSuccess = () => {
+        setEditForm(false);
+        setAddForm(false);
+    }
+
+    const normalizeToData = (data) => {
+        return Object.values(data);
+    }
+
+    const items = normalizeToData(data).map((item) => {
         const { id, name, slug, price, category, image, thumbnail } = item;
         return (
             <label key={id} className="ingredient">
@@ -79,10 +95,21 @@ const Ingredients = () => {
     });
 
     if(editForm) {
-        return <EditForm handleCancel={handleCancel}/>
+        return <EditForm 
+                    handleCancel={handleCancel}
+                    handleSuccess={handleSuccess}
+                    editData={data[values]}
+                />
     }
 
-    if(!editForm) {
+    if(addForm) {
+        return <AddForm 
+                    handleCancel={handleCancel}
+                    handleSuccess={handleSuccess}
+                />
+    }
+
+    if(!editForm && !addForm) {
         return (
             <div className="main flex-column">
                 <form className="flex flex-column">
@@ -90,15 +117,15 @@ const Ingredients = () => {
                         {items}
                     </div>
                 </form>
-                <div className="edit-buttons">
+                <div className="buttons-row ml-a mt-30">
                     <button 
                         {...({disabled: btnFlag})} 
                         onClick={handleEdit} 
-                        className="btn">Edit
+                        className="btn">Редактировать
                     </button>
                     <button 
-                        {...({disabled: btnFlag})} 
-                        className="btn">Remove
+                        onClick={handleAdd}
+                        className="btn">Создать
                     </button>
                 </div>
             </div>
