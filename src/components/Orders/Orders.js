462c41e63d2ee79958464350ba51bb75baa41ce7
Borisenko Anton <aborisenko@xbsoftware.by>
2021-08-14 21:21:54 +0300

    [update] orders page view

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -22,6 +22,20 @@ const Orders = () => {
         loadData();
     }, []);
 
+    if (isLoading) {
+        return (
+            <div className="main spinner">
+                <Loader
+                    type="Oval"
+                    color="#22CBF5"
+                    height={50}
+                    width={50}
+                    timeout={30000}
+                />
+            </div>
+        );
+    }
+
     if(error) {
         return <h1>ERROR: {error.message}</h1>
     }
@@ -29,36 +43,24 @@ const Orders = () => {
     return (
         <>
             <h1>Orders list:</h1>
-            { 
-                isLoading ? <div className="spinner flex">
-                    <Loader
-                        type="Oval"
-                        color="#22CBF5"
-                        height={20}
-                        width={20}
-                        timeout={30000}
-                    />
-                </div> :
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>ID: </th>
-                            <th>Размер: </th>
-                            <th>Тесто: </th>
-                            <th>Соус: </th>
-                            <th>Ингредиенты: </th>
-                            <th>Имя: </th>
-                            <th>Номер карты: </th>
-                            <th>Адрес: </th>
-                            <th>Цена: </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <OrderItem data={data} />
-                    </tbody>
-                </table>
-            }
-            {JSON.stringify(data)}
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>ID: </th>
+                        <th>Размер: </th>
+                        <th>Тесто: </th>
+                        <th>Соус: </th>
+                        <th>Ингредиенты: </th>
+                        <th>Имя: </th>
+                        <th>Номер карты: </th>
+                        <th>Адрес: </th>
+                        <th>Цена: </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <OrderItem data={data} />
+                </tbody>
+            </table>
         </>
     );
 }
